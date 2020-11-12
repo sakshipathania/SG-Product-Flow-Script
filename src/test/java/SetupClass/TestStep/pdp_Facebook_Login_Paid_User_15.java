05672b319215a856ccb3f1c700cd93bc4838f70f
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-11-12 10:43:07 +0530

    Update pdp_Facebook_Login_Paid_User_15.java

--- src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_15.java
+++ src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_15.java
@@ -16,29 +16,15 @@ public class pdp_Facebook_Login_Paid_User_15 extends Set {
 	
 	WebDriverWait wait = new WebDriverWait(driver,50);
 	 JavascriptExecutor js = (JavascriptExecutor) driver;
-	
-	@Given("^user is already on pdp page FP xv$")
-	public void user_is_already_on_pdp_page_FP_xv() throws Throwable {
-	    
-		driver.get("https://www.slideteam.net/adm-risk-monitoring-and-management-techniques-2018.html");
-		Thread.sleep(2000);
-		
-	}
 
-	@Then("^User click on Download button to download the product xv$")
-	public void user_click_on_Download_button_to_download_the_product_xv() throws Throwable {
+	@Given("^user is already on Home Page old fb$")
+	public void user_is_already_on_Home_Page_old_fb() throws InterruptedException {
 		
-		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickable(By.id("clicking")));
-		js.executeScript("arguments[0].scrollIntoView();",download_btn_pdp_fp);
-		Thread.sleep(2000);
-		download_btn_pdp_fp.click();
-		Thread.sleep(2000);
-	    
-	}
+		driver.get(AppURL);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		log.info("It's opening the website URL");
+		Thread.sleep(5000);
 
-	@Then("^user is redirected to Login page xv$")
-	public void user_is_redirected_to_Login_page_xv() throws Throwable {
-		Thread.sleep(4500);
 		try {
 			driver.findElement(By.cssSelector("div.social-login-authentication-channel:nth-child(3) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)")).click();
 			Thread.sleep(2000);
@@ -46,74 +32,82 @@ public class pdp_Facebook_Login_Paid_User_15 extends Set {
 		} 
 		catch (NoSuchElementException popup) {
 		}
-	}
-
-	@Then("^User click on sign in with facebook button xv$")
-	public void user_click_on_sign_in_with_facebook_button_xv() throws Throwable {
 	    
-		// WebElement fb_login_link = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='btn btn-block btn-social btn-facebook social-btn']")));
-		 //Thread.sleep(2000);
-		// fb_login_link.click();
-		Thread.sleep(3000);
-		//log.info("It's opening the website URL");
+	}
+
+	@Then("^User click on sign in with facebook button old fb$")
+	public void user_click_on_sign_in_with_facebook_button_old_fb() throws InterruptedException  {
 		
-		 WebElement fb_email = wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
-		 Thread.sleep(2000);
-		    fb_email.sendKeys("sumit.kumar@slidetech.in");
-		    Thread.sleep(2000);
-		    WebElement fb_pass = wait.until(ExpectedConditions.elementToBeClickable(By.id("pass")));
-		    Thread.sleep(2000);
-		    fb_pass.sendKeys("redhat2090");
-		    Thread.sleep(2000);
-		    WebElement fb_login_btn=wait.until(ExpectedConditions.elementToBeClickable(By.id("loginbutton")));
-		    Thread.sleep(2000);
-		    fb_login_btn.click();
-		    Thread.sleep(2000);
-	}
-
-	@Then("^user will be redirected to same page  xv$")
-	public void user_will_be_redirected_to_same_page_xv() throws Throwable {
-	    Thread.sleep(2000);
-         driver.get("https://www.slideteam.net/adm-risk-monitoring-and-management-techniques-2018.html");
-		Thread.sleep(2000);
-	}
-
-	@Then("^user download the product xv$")
-	public void user_download_the_product_xv() throws Throwable {
-	    Thread.sleep(1000);
-           
-		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickable(By.id("clicking")));
-		js.executeScript("arguments[0].scrollIntoView();",download_btn_pdp_fp);
-		Thread.sleep(2000);
-		download_btn_pdp_fp.click();
-		Thread.sleep(2000);
 		driver.get("https://www.slideteam.net/");
+		Thread.sleep(4000);
+		WebElement fb_link = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='btn btn-block btn-social btn-facebook social-btn']")));
+		Thread.sleep(2000);
+	    fb_link.click();
+		Thread.sleep(3000);
+		
+
+	    WebElement fb_email = wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
+		Thread.sleep(2000);
+	    fb_email.sendKeys("slidetech.qa@gmail.com");
+		Thread.sleep(2000);
+
+	    WebElement fb_pass = wait.until(ExpectedConditions.elementToBeClickable(By.id("pass")));
+		Thread.sleep(2000);
+	    fb_pass.sendKeys("himanshi@123");
+		Thread.sleep(2000);
+
+	    
+	    WebElement fb_login_btn=wait.until(ExpectedConditions.elementToBeClickable(By.id("loginbutton")));
+		Thread.sleep(2000);
+
+	    fb_login_btn.click();
+		
 	}
 
-	@Then("^user logout from website xv$")
-	public void user_logout_from_website_xv() throws Throwable {
-	   
-		Thread.sleep(1000);
+	@Then("^user go to free ppts page old fb$")
+	public void user_go_to_free_ppts_page_old_fb() throws InterruptedException  {
+	 
+		//free ppt pg
+		Thread.sleep(5000);
+		Actions actions = new Actions(driver);
+		WebElement free_ppt_btn1=wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Free Stuff")));
+		actions.moveToElement(free_ppt_btn1).moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Free PPTs')]"))).click().build().perform();
+		Thread.sleep(7000);
+
+	}
+
+	@Then("^user download a free ppt old fb$")
+	public void user_download_a_free_ppt_old_fb() throws InterruptedException  {
+	 
+		Thread.sleep(2000);
+		/*Thread.sleep(2000);
+		driver.findElement(By.cssSelector("li.product:nth-child(4) > div:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > span:nth-child(1) > a:nth-child(1)")).click();
+		Thread.sleep(3000); */
+		
+		driver.get("https://www.slideteam.net/circular-flow-of-process-4-stages-powerpoint-slides-templates.html");
+		Thread.sleep(3000);
+		
+		
+		driver.findElement(By.cssSelector("#clicking")).click();
+		Thread.sleep(3000);
+		
+		driver.navigate().refresh();
+		
+		// logout
+		Thread.sleep(2000);
 		try {
 			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
 			if (logout.isEnabled()) {
+				Thread.sleep(1000);
 				logout.click();
 				Thread.sleep(8000);
 				driver.navigate().refresh();
 				Thread.sleep(2000);
 			}
 		} catch (NoSuchElementException Ext) {
-		    Thread.sleep(2000);
-
 
 		}
 		
-		
-		driver.get("https://www.facebook.com");
-		Thread.sleep(5000);
-	    driver.manage().deleteAllCookies();
-	    Thread.sleep(7000);
-	    
 	}
 
 }
